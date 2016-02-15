3f09b0d8800d81dfd0ade7d7f77c99a2efb031b0
Ludovic Barman <ludovic.barman@gmail.com>
2016-02-15 15:41:17 +0100

    Default config file

---
+++ ajax/config.php
@@ -0,0 +1,12 @@
+<?php
+define("DEFAULT_DB_HOST", "localhost");
+define("DEFAULT_DB_USER", "---");
+define("DEFAULT_DB_USER_PASSWORD", "---");
+define("DEFAULT_DB_SELECTED", "---");
+
+define("AUTH_USER", "---");
+define("AUTH_PWD_SHA1", "---");
+
+define("BACKUP_EMAIL", "---@gmail.com");
+define("BACKUP_SUBJECT", "[VAULTAGE] Backup");
+?>
