c2946c4bff9f85f2a1858f3afb30484309814140
Ludovic Barman <ludovic.barman@gmail.com>
2016-02-24 13:48:33 +0100

    Gitignore, empty config

--- ajax/config.php
+++ ajax/config.php
@@ -1,12 +1,15 @@
 <?php
+
 define("DEFAULT_DB_HOST", "localhost");
-define("DEFAULT_DB_USER", "---");
-define("DEFAULT_DB_USER_PASSWORD", "---");
-define("DEFAULT_DB_SELECTED", "---");
+define("DEFAULT_DB_USER", "root");
+define("DEFAULT_DB_USER_PASSWORD", "");
+define("DEFAULT_DB_SELECTED", "vaultage");
+
 
 define("AUTH_USER", "---");
 define("AUTH_PWD_SHA1", "---");
 
 define("BACKUP_EMAIL", "---@gmail.com");
 define("BACKUP_SUBJECT", "[VAULTAGE] Backup");
+
 ?>
