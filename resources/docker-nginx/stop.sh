c90dfb9955966aacda176583285299b517c277e4
Ludovic Barman <lbarman@protonmail.ch>
2017-05-28 15:08:22 +0200

    UI fix

--- resources/docker-nginx/stop.sh
+++ resources/docker-nginx/stop.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# pretty colored message
+colors="true"
+okMsg="\033[32m[ok]${highlightOff}"
+if [ "$colors" = "false" ]; then
+    okMsg="[ok]"
+fi
+
 # Prevents wrong pwd when calling this script from another dir
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
@@ -8,3 +15,5 @@ cd "$SCRIPTPATH"
 . ./env
 
 docker-compose down
+
+echo "${okMsg} Vaultage docker stopped."
