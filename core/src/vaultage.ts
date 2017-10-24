06572f368260185f93851c410973aea6198a1b84
Hadrien Milano <hadrien.milano@gmail.com>
2017-10-24 21:03:59 +0200

    core api outline

--- core/src/vaultage.ts
+++ core/src/vaultage.ts
@@ -1,6 +1,48 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
 
+export class Vault {
+
+    public async login(
+        serverURL: string,
+        username: string,
+        masterPassword: string,
+    ): Promise<void> {
+
+    }
+
+    public logout() {
+
+    }
+
+    public count() {
+
+    }
+
+    public add() {
+
+    }
+
+    public remove() {
+
+    }
+
+    public find() {
+
+    }
+
+    public getAll() {
+
+    }
+
+    public update() {
+
+    }
+
+    public get() {
+
+    }
+
+    public isAuth() {
+
+    }
 }
+
