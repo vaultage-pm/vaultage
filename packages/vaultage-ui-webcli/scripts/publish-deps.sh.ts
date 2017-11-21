93474390a235cbc4e2183219b84073f266cf3bea
Hadrien Milano <hadrien.milano@gmail.com>
2017-11-21 21:27:37 +0100

    add missing issue links

--- packages/vaultage-ui-webcli/scripts/publish-deps.sh.ts
+++ packages/vaultage-ui-webcli/scripts/publish-deps.sh.ts
@@ -1,5 +1,5 @@
 import { cp, mkdir } from 'shelljs';
 
-// TODO: If not exists, run a build on vaultage-client to generate artifact
+// TODO (#111): If not exists, run a build on vaultage-client to generate artifact
 mkdir('-p', 'public/dist');
 cp(require.resolve('vaultage-client/dist/vaultage.js'), 'public/dist/');
