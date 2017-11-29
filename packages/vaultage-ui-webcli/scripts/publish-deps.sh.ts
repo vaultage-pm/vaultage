940547cad00b26600646f41cdd41002ad96c8671
Ludovic Barman <lbarman@protonmail.ch>
2017-11-29 21:28:46 +0100

    Removes the unneeded copy script in UI

--- packages/vaultage-ui-webcli/scripts/publish-deps.sh.ts
+++
@@ -1,5 +0,0 @@
-import { cp, mkdir } from 'shelljs';
-
-// TODO (#111): If not exists, run a build on vaultage-client to generate artifact
-mkdir('-p', 'public/dist');
-cp(require.resolve('vaultage-client/dist/vaultage.js'), 'public/dist/');
