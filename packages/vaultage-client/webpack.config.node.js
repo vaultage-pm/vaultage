04931632d5961e9dac63db9f49480c7d04b69fcc
Hadrien Milano <hadrien.milano@appdynamics.com>
2018-06-29 18:08:19 -0700

    fix integration test

--- packages/vaultage-client/webpack.config.node.js
+++
@@ -1,9 +0,0 @@
-const baseConfig = require("./webpack.config.base");
-
-module.exports = baseConfig.merge({
-    output: {
-        filename: 'dist/vaultage.js',
-        chunkFilename: 'dist/[name].bundle.js',
-        libraryTarget: 'commonjs'
-    },
-});
