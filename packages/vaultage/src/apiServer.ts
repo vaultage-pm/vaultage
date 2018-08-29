1f9d50363a2180056b82a8ea5e20d2750f20a224
Ludovic Barman <lbarman@protonmail.ch>
2018-08-29 14:24:45 +0200

    Increase limit for POST to 5mb

--- packages/vaultage/src/apiServer.ts
+++ packages/vaultage/src/apiServer.ts
@@ -1,8 +1,10 @@
 import { json } from 'body-parser';
+import 'body-parser';
 import * as cors from 'cors';
 import * as express from 'express';
 import * as path from 'path';
 import { useExpressServer } from 'routing-controllers';
+import { EXPRESS_SERVER_POST_LIMIT } from './constants';
 
 /**
  * Creates an express server serving the Vaultage API (used to save and retreive the encrypted passwords).
@@ -11,6 +13,10 @@ import { useExpressServer } from 'routing-controllers';
  */
 export function createVaultageAPIServer(): express.Application {
     const expressServer = express();
+
+    // Increase the limit of POST queries (needed as the database grows quite large)
+    const bodyParser = require('body-parser');
+    expressServer.use(bodyParser.json({limit: EXPRESS_SERVER_POST_LIMIT}));
 
     // Allow requests from all origins.
     // We can do this because we don't have actual sessions and there is nothing more to be obtained
--- packages/vaultage/src/constants.ts
+++ packages/vaultage/src/constants.ts
@@ -1,3 +1,5 @@
 export const CONFIG_FILENAME = 'config.json';
 
 export const VAULT_FILENAME = 'vault.json';
+
+export const EXPRESS_SERVER_POST_LIMIT = '5mb';
