f0281761dfa863b7a58f75a31d2d3c5a34b97300
Ludovic Barman <ludovic.barman@protonmail.com>
2019-10-14 22:13:05 +0200

    Print current config on 'config get'

--- packages/vaultage-ui-webcli/src/commands/Config.ts
+++ packages/vaultage-ui-webcli/src/commands/Config.ts
@@ -30,7 +30,7 @@ export class ConfigCommand implements ICommand {
                 this.set(this.parseArg('key', args[1]), this.parseArg('value', args[2]));
                 break;
             case 'get':
-                this.get(this.parseArg('key', args[1]));
+                this.get(this.parseArg('key', args[1], false));
                 break;
             case 'clear':
                 this.clear(this.parseArg('key', args[1]));
@@ -51,8 +51,9 @@ export class ConfigCommand implements ICommand {
 
     private printUsage() {
         this.shell.echo('Usage: config <set|get|clear> key [value]');
-        this.shell.echo('');
         this.shell.echo('keys: ' + Object.keys(AVAILABLE_OPTIONS).join(','));
+        this.shell.echo('');
+        this.shell.echo('Type `config get` to see current config.');
     }
 
     private clear(key: string) {
@@ -67,16 +68,26 @@ export class ConfigCommand implements ICommand {
         if (configName === 'sessionTimeout') {
             this.timeout.validateTimeoutFormat(value);
         }
+        let previousValue = this.config[configName];
         this.config.write(configName, value);
+        let newValue = this.config[configName];
+                
         if (configName === 'sessionTimeout') {
             this.timeout.resetTimeout();
         }
-        this.shell.echo('OK');
+        this.shell.echo(`OK, previous value was ${previousValue}, new value is ${newValue}.`);
     }
 
     private get(key: string) {
-        const configName = this.convertKeyToConfigEntry(key);
-        this.shell.echo(String(this.config[configName]));
+        if (key == "") {
+            for(let key of Object.keys(AVAILABLE_OPTIONS)) {
+                const configName = this.convertKeyToConfigEntry(key);
+                this.shell.echo(`${key} => ${String(this.config[configName])}`);
+            }
+        } else {
+            const configName = this.convertKeyToConfigEntry(key);
+            this.shell.echo(String(this.config[configName]));
+        }
     }
 
     private convertKeyToConfigEntry(key: string): keyof Config {
@@ -87,11 +98,14 @@ export class ConfigCommand implements ICommand {
         return setting;
     }
 
-    private parseArg(name: string, value: string | undefined): string {
-        if (!value) {
-            this.printUsage();
-            throw new Error(`Missing argument '${name}'.`);
+    private parseArg(name: string, value: string | undefined, required: boolean=true): string {
+        if (value !== undefined) {
+            if(required) {
+                this.printUsage();
+                throw new Error(`Missing argument '${name}'.`);
+            }
+            return value;
         }
-        return value;
+        return '';
     }
 }
