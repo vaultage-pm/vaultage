4f7038bb861fb9da2230d40a11c0f87f17ad1566
Pedro <pedro.moreira01@fatec.sp.gov.br>
2021-02-28 11:17:03 -0300

    Adding pin lock encryption.

--- packages/vaultage-pwa/src/app/pin-lock.service.spec.ts
+++ packages/vaultage-pwa/src/app/pin-lock.service.spec.ts
@@ -4,6 +4,21 @@ import { anyString, Mock, reset, when } from 'omnimock';
 import { PinLockService } from './pin-lock.service';
 import { LOCAL_STORAGE } from './platform/providers';
 
+/*
+decrypted mock data:
+{
+    "pin": "1234",
+    "data": "53cr37"
+}
+-------
+encrypted mock data:
+[
+    '7534e92f7cff82815e997e2b2e8d65421f8f0482344b18d44c50f4745d7ee7f85f3fca088e7dfdcb6bbca5582a1869d1e597cea27e19e1b3e124ccf5d2fd81b5',
+    '7d80a5a2b8b1ba326fdf5feeacfd00b52e4ca9cd8e831d41d823dcbfebbd1ccd'
+]
+
+*/
+
 
 describe('PinLockServiceTest', () => {
 
@@ -18,10 +33,11 @@ describe('PinLockServiceTest', () => {
     it('setSecret sets the secret', () => {
         when(lsMock.setItem(anyString(), anyString())).call((key, datum) => {
             expect(key).toBe('vaultage_locked');
-            expect(JSON.parse(datum)).toEqual({
-                pin: '1234',
-                data: '53cr37'
-            });
+
+            expect(JSON.parse(datum)).toEqual([
+                "7534e92f7cff82815e997e2b2e8d65421f8f0482344b18d44c50f4745d7ee7f85f3fca088e7dfdcb6bbca5582a1869d1e597cea27e19e1b3e124ccf5d2fd81b5",
+                "7d80a5a2b8b1ba326fdf5feeacfd00b52e4ca9cd8e831d41d823dcbfebbd1ccd"
+            ]);
         }).once();
         service.setSecret('1234', '53cr37');
     });
@@ -29,10 +45,11 @@ describe('PinLockServiceTest', () => {
     it('getSecret with good pin returns the secret', () => {
         when(lsMock.getItem(anyString())).call(key => {
             expect(key).toBe('vaultage_locked');
-            return `{
-                "pin": "1234",
-                "data": "53cr37"
-            }`;
+
+            return `[
+                "7534e92f7cff82815e997e2b2e8d65421f8f0482344b18d44c50f4745d7ee7f85f3fca088e7dfdcb6bbca5582a1869d1e597cea27e19e1b3e124ccf5d2fd81b5",
+                "7d80a5a2b8b1ba326fdf5feeacfd00b52e4ca9cd8e831d41d823dcbfebbd1ccd"
+            ]`;
         });
         expect(service.getSecret('1234')).toBe('53cr37');
     });
@@ -40,10 +57,11 @@ describe('PinLockServiceTest', () => {
     it('getSecret with bad pin returns undefined', () => {
         when(lsMock.getItem(anyString())).call(key => {
             expect(key).toBe('vaultage_locked');
-            return `{
-                "pin": "1234",
-                "data": "53cr37"
-            }`;
+
+            return `[
+                "7534e92f7cff82815e997e2b2e8d65421f8f0482344b18d44c50f4745d7ee7f85f3fca088e7dfdcb6bbca5582a1869d1e597cea27e19e1b3e124ccf5d2fd81b5",
+                "7d80a5a2b8b1ba326fdf5feeacfd00b52e4ca9cd8e831d41d823dcbfebbd1ccd"
+            ]`;
         });
         expect(service.getSecret('4321')).toBe(undefined);
     });
--- packages/vaultage-pwa/src/app/pin-lock.service.ts
+++ packages/vaultage-pwa/src/app/pin-lock.service.ts
@@ -1,6 +1,9 @@
 import { Inject, Injectable } from '@angular/core';
 import { LOCAL_STORAGE } from './platform/providers';
 
+import { sha384, sha512 } from 'js-sha512';
+import { ModeOfOperation, utils as AESUtil } from "aes-js";
+
 const STORAGE_KEY = 'vaultage_locked';
 
 @Injectable()
@@ -14,26 +17,64 @@ export class PinLockService {
     }
 
     public setSecret(pin: string, data: string): void {
-        this.ls.setItem(STORAGE_KEY, JSON.stringify({pin, data}));
+
+        this.ls.setItem(STORAGE_KEY, JSON.stringify(this.encrypt(JSON.stringify({pin, data}), pin)));
+    }
+
+
+    private encrypt(data: string, pin: string): string[] {
+        data = this.checkMultipleSize(data, 16);
+        const pinHash = sha384(pin);
+        const iv = AESUtil.hex.toBytes(pinHash.substr(0, 32));
+        const key = AESUtil.hex.toBytes(pinHash.substr(32, 64));
+        const crytoData = AESUtil.hex.fromBytes(new ModeOfOperation.cbc(key, iv).encrypt(
+            AESUtil.utf8.toBytes(data)));
+        const checksum = sha512(pinHash + data);
+        return [checksum,crytoData];
+    }
+    private checkMultipleSize(data: string, sizeMultiple: number): string {
+        while(data.length % sizeMultiple > 0) {
+            data += ' ';
+        }
+        return data;
     }
 
     public getSecret(userPin: string): string | undefined {
         const storage = this.getStorage();
-        if (storage != null && storage.pin === userPin) {
-            return storage.data;
+
+        if (storage != null) {
+            const pinStorage = this.checkDecryption(storage, userPin);
+            if(!pinStorage) {
+                return undefined;
+            }
+            return pinStorage.data;
         }
+    }
+    private checkDecryption(storageAsString: string, userPin: string): PinStorage | undefined {
+        const storage: string[] = JSON.parse(storageAsString);
+        const pinHash = sha384(userPin);
+        const iv = AESUtil.hex.toBytes(pinHash.substr(0, 32));
+        const key = AESUtil.hex.toBytes(pinHash.substr(32, 64));
+        const data = AESUtil.utf8.fromBytes(new ModeOfOperation.cbc(key, iv).decrypt(
+            AESUtil.hex.toBytes(storage[1])));
+        const checksum = sha512(pinHash + data);
+        if(checksum == storage[0]) {
+            return JSON.parse(data);
+        }
+        return undefined;
     }
 
     public reset(): void {
         this.ls.removeItem(STORAGE_KEY);
     }
 
-    private getStorage(): PinStorage | undefined {
+
+    private getStorage(): string | undefined {
         const data = this.ls.getItem(STORAGE_KEY);
         if (data === null) {
             return undefined;
         }
-        return JSON.parse(data);
+        return data;
     }
 }
 
